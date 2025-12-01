8b60ffd2af335de81398106a9b4c0ce40af0454d
mlc-suzuki-ryota <suzuki-ryota@mitsue.co.jp>
2025-12-01 14:03:32 +0900

    switch lazygit

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,11 @@
 return {
   'nvim-mini/mini.nvim',
   config = function()
+    require("mini.starter").setup()
     require("mini.cursorword").setup()
     require("mini.comment").setup()
-    require("mini.indentscope").setup({
-      symbol = '│'
-    })
+    --    require("mini.indentscope").setup({
+    --      symbol = '│'
+    --    })
   end
 }
