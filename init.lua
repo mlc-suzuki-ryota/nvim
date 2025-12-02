d0671650fc0c4ed7c5045125d3e722d2e89bc423
mlc-suzuki-ryota <suzuki-ryota@mitsue.co.jp>
2025-12-02 12:04:36 +0900

    open default yazi

--- init.lua
+++ init.lua
@@ -1,18 +1,7 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 require("config.options")
 require("config.lazy")
 require("config.lsp")
 require("config.keymaps")
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  group = vim.api.nvim_create_augroup("YaziGroup", { clear = true }),
-  pattern = "*",
-  callback = function()
-    if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
-      vim.cmd("Yazi")
-    end
-  end,
-  desc = "ディレクトリを開いた時にYaziを起動する",
-})
--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -5,5 +5,14 @@ return {
   dependencies = {
     { "nvim-lua/plenary.nvim", lazy = true },
   },
+  opts = {
+    open_for_directories = true,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
 
+  init = function()
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }
