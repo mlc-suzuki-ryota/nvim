4b8d871919b1fad19cabadf5b5d32f7bfe49d02e
mlc-suzuki-ryota <suzuki-ryota@mitsue.co.jp>
2025-12-02 11:47:28 +0900

    yazi plugin change

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,3 +1,9 @@
 return {
-  'yukimura1227/vim-yazi'
+  "mikavilpas/yazi.nvim",
+  version = "*", -- use the latest stable version
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+
 }
