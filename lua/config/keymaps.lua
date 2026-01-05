cdd385d9034ec803b5131a1f42439b92a382dc8b
mlc-suzuki-ryota <suzuki-ryota@mitsue.co.jp>
2026-01-05 16:15:38 +0900

    leader key setting split

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,13 +13,13 @@ keymap.set("n", "te", ":tabedit")
 keymap.set("n", "tn", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+keymap.set("n", "<Leader>ss", ":split<Return>", opts)
+keymap.set("n", "<Leader>sv", ":vsplit<Return>", opts)
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+keymap.set("n", "<Leader>sh", "<C-w>h")
+keymap.set("n", "<Leader>sk", "<C-w>k")
+keymap.set("n", "<Leader>sj", "<C-w>j")
+keymap.set("n", "<Leader>sl", "<C-w>l")
 
 -- Resize window
 keymap.set("n", "<C-w><left>", "<C-w><")
